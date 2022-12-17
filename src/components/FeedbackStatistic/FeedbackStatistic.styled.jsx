f62aab140f9b68c7a4b34576b9142aa9eaba347b
Oleksandr P <goodwin009@gmail.com>
2022-12-17 16:03:12 +0200

    task 2 completed

---
+++ src/components/FeedbackStatistic/FeedbackStatistic.styled.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const StatisticList = styled.ul`
+  list-style: none;
+  /* display: flex; */
+  margin: 0;
+  /* justify-content: flex-start; */
+  text-align: left;
+  padding: 0 16px;
+  gap: 16px;
+`;
+
+// export const SectionTitle = styled.h2`
+//   font-weight: 700;
+//   margin: 0;
+//   padding: 16px;
+//   text-align: left;
+
+//   font-family: 'Roboto', sans-serif;
+// `;
--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,14 +1,15 @@
 import styled from '@emotion/styled';
 
-export const StatisticsSection = styled.section`
-  width: 360px;
-  margin: 36px 0;
-  border: 1px solid rgb(198, 195, 195);
+export const SectionItem = styled.section`
+  width: 320px;
+  text-align: center;
 `;
 
-export const Title = styled.h2`
-  text-align: center;
-  color: rgb(96, 93, 93);
-  text-transform: uppercase;
-  font-size: 24px;
+export const SectionTitle = styled.h2`
+  font-weight: 700;
+  margin: 0;
+  padding: 16px;
+  text-align: left;
+
+  font-family: 'Roboto', sans-serif;
 `;
